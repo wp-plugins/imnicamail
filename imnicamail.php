6503238096ca23609531b035b3090e1e45b8a0e2
imnicamail <imnicamail@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-14 18:46:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/imnicamail/trunk@322807 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ imnicamail.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+    * Plugin Name: ImnicaMail
+    * Plugin URI: http://www.imnicamail.com/
+    * Description: This plugin adds a subsciption form to your blog, so that your viewers can subscribe easily to your mail list.
+    * Version: 0.1
+    * Author: ImnicaMail
+    * Author URI: http://www.imnicamail.com
+    */
+?>
